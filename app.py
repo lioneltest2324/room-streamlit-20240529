440da59cc9937da8c0a83cdefcfc3d65ba7d6df1
lioneltest2324 <120074740+lioneltest2324@users.noreply.github.com>
2024-06-07 11:31:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ st.set_page_config(layout="wide")
 show_pages(
     [
         Page("room-sku-7day.py", "7日"),
-        Page("room-sku-3day.py", "3日")
+        Page("room-sku-3day.py", "3日"),
+        Page("room-sku-custom-day.py", "自选日期")
 
     ]
 )
